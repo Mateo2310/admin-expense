39a5da32bcd9250331500603e6148a2ac8f52297
Mateo Villanueva <mateo.villanueva@nubiral.com>
2025-03-22 19:59:14 -0300

    init commit

---
+++ src/main/java/com/admin_expenses/admin_expenses/AdminExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.admin_expenses.admin_expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admin_expenses/admin_expenses/AdminExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin_expenses.admin_expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
